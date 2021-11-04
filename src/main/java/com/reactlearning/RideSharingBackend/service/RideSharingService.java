1dc723a6684c3dd863ac3ae87c1081dca079e52b
mhal1 <muhamedhalimi@hotmail.com>
2021-11-04 21:01:30 +0000

    Ride Share Backend as at 04/11/2021.

--- src/main/java/com/reactlearning/RideSharingBackend/service/RideSharingService.java
+++ src/main/java/com/reactlearning/RideSharingBackend/service/RideSharingService.java
@@ -11,7 +11,6 @@ import com.reactlearning.RideSharingBackend.repository.RideSharingRepository;
 import com.reactlearning.RideSharingBackend.repository.RidesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
